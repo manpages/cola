50fffeebe642d129f03f026c9fde9316794469e3
Jonn Mostovoy <amarr.industrial@gmail.com>
2015-10-25 19:40:45 +0100

    Global variables ftw :3

--- cola/www/js/index.js
+++ cola/www/js/index.js
@@ -1,73 +1,56 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var gPosition
+var gGPSLoop
+
+var view = function() {
+  return document.body.querySelector('.app__mainMenu--view')
+}
+
+var positionToString = function(position) {
+  return ('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n') 
+}
+
+var beamGPS = function() {
+  navigator.geolocation.getCurrentPosition( function(x) {
+                                              gPosition = x
+                                              view().innerText = positionToString(gPosition)
+                                            }
+                                          , function() {} )
+}
+
 var app = {
-    // Application Constructor
+
     initialize: function() {
-        this.bindEvents();
-        // This method accepts a Position object, which contains the
-        // current GPS coordinates
-        //
-        var onSuccess = function(position) {
-            alert('Latitude: '          + position.coords.latitude          + '\n' +
-                  'Longitude: '         + position.coords.longitude         + '\n' +
-                  'Altitude: '          + position.coords.altitude          + '\n' +
-                  'Accuracy: '          + position.coords.accuracy          + '\n' +
-                  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-                  'Heading: '           + position.coords.heading           + '\n' +
-                  'Speed: '             + position.coords.speed             + '\n' +
-                  'Timestamp: '         + position.timestamp                + '\n');
-        };
-
-        // onError Callback receives a PositionError object
-        //
-        function onError(error) {
-            alert('code: '    + error.code    + '\n' +
-                  'message: ' + error.message + '\n');
-        }
-
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        this.bindEvents()
+        // We're working only in foreground to drain less battery
+        // so we fetch GPS data pretty rigorously.
+        gGPSLoop = window.setInterval(beamGPS, 666) 
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('deviceready', this.onDeviceReady, false)
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        app.receivedEvent('deviceready')
     },
-    // Update DOM on a Received Event
+
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        var parentElement = document.getElementById(id)
+        var listeningElement = parentElement.querySelector('.listening')
+        var receivedElement = parentElement.querySelector('.received')
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        listeningElement.setAttribute('style', 'display:none;')
+        receivedElement.setAttribute('style', 'display:block;')
 
-        console.log('Received Event: ' + id);
+        console.log('Received Event: ' + id)
     }
 };
 
-app.initialize();
+app.initialize()
